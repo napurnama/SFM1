6f4651a223d769d8ccbd584da83aec7f399ac25c
Naufal Adli Purnama <purnama.naufal.adli@gmail.com>
2022-02-25 06:48:33 +0300

    added scripts

--- scriptMenit.sh
+++ scriptMenit.sh
@@ -2,7 +2,7 @@ mkdir -p ~/log
 
 str1=$(free -m | awk 'BEGIN{OFS=","} NR!=1 {for(i=2;i<=NF;i++) printf $i",";}')
 str2=$(du -sh ~ | awk 'BEGIN{OFS=","} {print $2,$1}')
-dtFull=$(date +%F | awk -F"-" 'BEGIN{OFS=""}{print $1$2$3}')
+dtFull=$(date +%F | awk -F"-" '{print $1$2$3}')
 dtTime=$(date +"%X" | awk 'BEGIN{FS=":"} {print $1$2$3}')
 dtHour=$(date +"%X" | awk 'BEGIN{FS=":"} {print $1}')
 
@@ -11,6 +11,7 @@ then
 	chmod 600 ~/log/metrics_agg_$dtFull$dtHour.log
 fi
 echo "$str1$str2" >> ~/log/metrics_$dtFull$dtTime.log
+echo "$str1$str2"
 cat ~/log/metrics_$dtFull$dtTime.log >> ~/log/metrics_agg_$dtFull$dtHour.log
 
 chmod 400 ~/log/metrics_agg_$dtFull$dtHour.log
